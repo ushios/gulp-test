28cade07b037be945372d53cc545f8865e7a2023
UshioShugo <ushio.s@gmail.com>
2014-08-18 16:05:44 +0900

    add csso.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp');
 var gulputil = require('gulp-util');
+
+// css
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
+var csso = require('gulp-csso');
 
 gulp.task('default',['css'], function(){
 
@@ -11,5 +14,6 @@ gulp.task('css', function () {
     gulp.src('./assets/scss/*.scss')
     .pipe(sass())
 	.pipe(minifyCss())
+    .pipe(csso())
     .pipe(gulp.dest('./assets/css')); 
 });
